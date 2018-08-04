0577d31c6f08648821a51ad5bc2bac13c556eb37
Adrian Llerena <a.llerena001@gmail.com>
2018-08-04 11:50:03 -0400

    new comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MyNestedComponent } from './my-nested/my-nested.component';
+import { MyPropertyBindingComponent } from './my-property-binding/my-property-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyNestedComponent
+    MyNestedComponent,
+    MyPropertyBindingComponent
   ],
   imports: [
     BrowserModule
